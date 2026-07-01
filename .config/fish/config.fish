8dd9879fe18eaa4b8bc54a0c3b2e8969a8b0210f
Erik Karlgren Domercq <erik.karldom@gmail.com>
2026-07-01 10:51:13 +0200

    feat(fish): install fisher in interactive shell if not installed

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -24,6 +24,9 @@ if status is-interactive
     # Do not bind ctrl-v to search variables with fzf
     fzf_configure_bindings --variables=
 
+    if not functions -q fisher
+        curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+    end
 end
 
 set -x EDITOR "nvim"
